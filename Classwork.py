60c915dd935e639b848870e0e0b7b58595f44c18
Jake Brulato <jakebrulato@gmail.com>
2023-11-20 15:58:36 -0500

    Update Classwork.py

--- Classwork.py
+++ Classwork.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 mt= pd.read_csv('/Users/jakebrulato/Downloads/Midterm Data.csv')
 
-st.write("# Welcome to the example of my tableau dataframe👋")
+st.write("# Welcome to the example of my dataframe👋")
 st.sidebar.success("Select a demo above.")
 
 
