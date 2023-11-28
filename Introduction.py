1f782aecf0af855c2bef191ed2bafa3568e80693
Jake Brulato <124078887+EviIius@users.noreply.github.com>
2023-11-28 02:22:30 -0500

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -18,7 +18,7 @@ eco = pd.DataFrame(df)
 
 #Converting Year from int64 to datetime
 # eco['Year'] = eco['Year'].astype('datetime64[ns]') <-- This one seems to be the issue
-eco['Year'] = pd.to_datetime(eco['Year'], format='%y%m%d')
+# eco['Year'] = pd.to_datetime(eco['Year'], format='%y%m%d')
 # eco['Year'] = eco['Year'].dt.year
 eco.info()
 
@@ -29,7 +29,16 @@ eco.info()
 #Format of page potentially
 st.write("# Welcome to the example of my dataframe👋")
 st.header("# It so far isn't much but whatever")
-st.sidebar.success("WIP.")
+
+#Optional Sidebar Just testing
+st.sidebar.header('Word Cloud Settings')
+max_word = st.sidebar.slider("Max Words",min_value=10,max_value=200,value=100, step=10)
+max_font = st.sidebar.slider("Size of largest Word",min_value=50,max_value=350,value=60, step=10)
+img_width = st.sidebar.slider("Image Width",min_value=100,max_value=800,value=600, step=10)
+random = st.sidebar.slider("Random State", min_value=30,max_value=100,value=20)
+remove_stop_words = st.sidebar.checkbox("Remove Stop Words?",value=True)
+st.sidebar.header('Word Count Settings')
+word_count = st.sidebar.slider("Minimum count of words",min_value=5,max_value=100,value=40, step=1)
 
 #Optional file uploader
 # user_file = st.file_uploader(
