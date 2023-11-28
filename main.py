3afa5b2673a692b2deecc98e77d2dc35ce9546a9
Jake Brulato <124078887+EviIius@users.noreply.github.com>
2023-11-28 01:45:55 -0500

    Added comments to main file

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-
+#Imports
 import pandas as pd
 import streamlit as st
 import pandas as pd
 import altair as alt
 import time
 
+#Page Configuration
 st.set_page_config(layout='wide')
 
+#Data Reading and URL Linkage to repository
 url = 'https://raw.githubusercontent.com/EviIius/TableauFinalP/main/economicdata2003-2021.csv'
 df = pd.read_csv(url, sep=',')
 
+
+#Creating a new dataframe different from original data
 eco = pd.DataFrame(df)
-# eco['Year'] = eco['Year'].astype('datetime64[ns]')
+
+#Converting Year from int64 to datetime
+# eco['Year'] = eco['Year'].astype('datetime64[ns]') <-- This one seems to be the issue
 eco['Year'] = pd.to_datetime(eco['Year'], format='%y%m%d')
 # eco['Year'] = eco['Year'].dt.year
 eco.info()
@@ -19,10 +25,13 @@ eco.info()
 
 # df['Year'] = pd.to_datetime(df['Year']).dt.strftime('%Y')
 # print(df['Year'])
+
+#Format of page potentially
 st.write("# Welcome to the example of my dataframe👋")
 st.header("# It so far isn't much but whatever")
 st.sidebar.success("WIP.")
 
+#Optional file uploader
 # user_file = st.file_uploader(
 #     'Select Your Local User CSV (default provided)')
 
@@ -31,8 +40,10 @@ st.sidebar.success("WIP.")
 # else:
 #     st.stop()
 
+#Displaying Dataframe
 st.dataframe(eco)
 
+#Caching the data
 
 # @st.cache_data()
 # def load_file(user_file):
